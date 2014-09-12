f74fe73206a5df784c62f704cf7c47138af7365f
Andre Kampert <andre@product-foundry.com>
2014-09-12 15:36:50 +0200

    Load NewTelco doc

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+
+$(document).ready(function () {
+    function resizeContent() {
+        $('iframe').height($(window).height());
+    }
+
+    resizeContent();
+
+    $(window).resize(function() {
+        resizeContent();
+    });
+
+});
