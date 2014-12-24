20da137979ab302af4cab375ebc16cd11693bd20
Daniel Codrea <daniel@product-foundry.com>
2014-12-24 14:59:06 +0200

    Removed old homepage and implemented new horizontal scroll multiple langugaes homepage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,55 +6,10 @@ $(document).ready(function(){
   var currentDate = new Date();
   $('#current-year').text((currentDate).getFullYear());
   
-  function cbpAnimatedHeader() {
-    var docElem = document.documentElement,
-      didScroll = false,
-      changeHeaderOn = 300;
-    function init() {
-      window.addEventListener( 'scroll', function( ) {
-        if( !didScroll ) {
-          didScroll = true;
-          setTimeout( scrollPage, 250 );
-        }
-      }, false );
-    }
-    function scrollPage() {
-      var sy = scrollY();
-      if ( sy >= changeHeaderOn ) {
-        $( '.navbar-default' ).addClass('navbar-shrink' );
-      }
-      else {
-        $( '.navbar-default' ).removeClass('navbar-shrink' );
-      }
-      didScroll = false;
-    }
-    function scrollY() {
-      return window.pageYOffset || docElem.scrollTop;
-    }
-    init();
-  }
-  
-  cbpAnimatedHeader();
-
-  var scrollToPosition = function(event) {
-    var target;
-    if (event !== null) {
-      event.preventDefault();
-    }
-    target = $(this).attr('href');
-    $('body').animate({
-      scrollTop: $(target).offset().top + 1
-    }, 500, function() {
-      window.location.hash = target;
-    });
-  };
-  $('#main-nav a').click(scrollToPosition);
-  
   $('.close-modal').click(function(){
      window.location=$(this).find('a').attr('href');
      return false;
   });
-  
   
   $('#mixedContent').smoothDivScroll({
 		hotSpotScrolling: false,
@@ -62,27 +17,23 @@ $(document).ready(function(){
     mousewheelScrolling: 'horizontal',
     scrollToAnimationDuration: 1200,
     scrollToEasingFunction: 'swing'
-    
 	});
   
   $('[data-toggle="offcanvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active');
   });
   
-//  $('a.scroll-horizontal').click(function() {
-//      var href = $.attr(this, 'href');
-//      $('#mixedContent').animate({
-//          scrollLeft: $(href).offset().left + 1
-//      }, 1500, 'swing', function () {
-//          window.location.hash = href;
-//      });
-//  });
-  
   $('.scroll-horizontal').each(function(){
     $(this).on('click', function(e){
       e.preventDefault();
       var jumpToVal = $(this).attr('data-jump-to');
       $('#mixedContent').smoothDivScroll('scrollToElement', 'id', jumpToVal);
+    });
+  });
+  
+  $('.sub-menu-link').each(function(){
+    $(this).on('click', function(e){
+      e.preventDefault();
       resetSubMenu();
       $(this).find('i').toggleClass('fa-chevron-up fa-chevron-down');
       $(this).next('.sub-menu').toggleClass('hidden');
@@ -90,9 +41,15 @@ $(document).ready(function(){
   });
   
   function resetSubMenu() {
-    $('.scroll-horizontal').each(function(){
+    $('.sub-menu-link').each(function(){
       $(this).find('i').removeClass('fa-chevron-up').addClass('fa-chevron-down');
       $(this).next('.sub-menu').addClass('hidden');
+    });
+  }
+  
+  if($(window).width() < 768) {
+    $('.scroll-horizontal').on('click', function(){
+      $('.row-offcanvas').removeClass('active');
     });
   }
 
